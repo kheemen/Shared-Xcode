6dcb6672ef0a67877180a6ccff7fe2f20a4cbbde
Shirly Cheng <siaolingcheng@Siaos-MacBook-Air.local>
2013-04-11 20:25:54 +0800

    commit test

--- Xcode proj/Project2/main.m
+++ Xcode proj/Project2/main.m
@@ -15,5 +15,5 @@ int main(int argc, char *argv[])
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
-    //comment test 4
+    //comment test 5
 }
